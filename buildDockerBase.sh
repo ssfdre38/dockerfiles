4d904bbc8251dfa05b5686e86f078d0f765267ea
Daniel Elliott <ssfdre38@msn.com>
2023-12-04 09:45:33 -0800

    updating github
    Updating GitHub repo to match my personal git server

--- buildDockerBase.sh
+++ buildDockerBase.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
-docker build --rm -f AMPDockerFile -t cubecoders/ampbase .
-docker build --rm -f AMPDockerFileJava -t cubecoders/ampbase:java .
-docker build --rm -f AMPDockerFileNode -t cubecoders/ampbase:node .
-docker build --rm -f AMPDockerFileXvfb -t cubecoders/ampbase:xvfb .
-docker build --rm -f AMPDockerFilePython3 -t cubecoders/ampbase:python3 .
-docker build --rm -f AMPDockerFileWine -t cubecoders/ampbase:wine .
-docker build --rm -f AMPDockerFileMono -t cubecoders/ampbase:mono .
+docker build --rm -f AMPDockerFile -t ssfdre38/ampdocker .
+docker build --rm -f AMPDockerFileJava -t ssfdre38/ampdocker:java .
+docker build --rm -f AMPDockerFileNode -t ssfdre38/ampdocker:node .
+docker build --rm -f AMPDockerFileXvfb -t ssfdre38/ampdocker:xvfb .
+docker build --rm -f AMPDockerFilePython3 -t ssfdre38/ampdocker:python3 .
+docker build --rm -f AMPDockerFileWine -t ssfdre38/ampdocker:wine .
+docker build --rm -f AMPDockerFileMono -t ssfdre38/ampdocker:mono .
+docker build --rm -f AMPDockerFileRedBot -t ssfdre38/ampdocker:RedBot .
+docker build --rm -f AMPDockerFileGo -t ssfdre38/ampdocker:Go .
+docker build --rm -f AMPDockerFileLang -t ssfdre38/ampdocker:Lang .
+docker build --rm -f AMPDockerFileGame -t ssfdre38/ampdocker:Game .
 
-#docker push cubecoders/ampbase
+#docker push ssfdre38/ampdocker
