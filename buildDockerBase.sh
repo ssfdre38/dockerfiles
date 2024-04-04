53456a82e23925d180d34fc07a192bc4bae828a8
Daniel Elliott <ssfdre38@msn.com>
2024-04-04 16:54:23 -0700

    Update to match private repo
    Update docker build files to match the main docker repo on https://git.danielelliott.online/ssfdre38/dockerfiles

--- buildDockerBase.sh
+++ buildDockerBase.sh
@@ -7,9 +7,9 @@ docker build --rm -f AMPDockerFilePython3 -t ssfdre38/ampdocker:python3 .
 docker build --rm -f AMPDockerFileWine -t ssfdre38/ampdocker:wine .
 docker build --rm -f AMPDockerFileMono -t ssfdre38/ampdocker:mono .
 docker build --rm -f AMPDockerFileRedBot -t ssfdre38/ampdocker:RedBot .
-docker build --rm -f AMPDockerFileGo -t ssfdre38/ampdocker:Go .
-docker build --rm -f AMPDockerFileLang -t ssfdre38/ampdocker:Lang .
-docker build --rm -f AMPDockerFileGame -t ssfdre38/ampdocker:Game .
+docker build --rm -f AMPDockerFileGo -t ssfdre38/ampdocker:go .
+docker build --rm -f AMPDockerFileLang -t ssfdre38/ampdocker:lang .
 docker build --rm -f AMPDockerFilePython312 -t ssfdre38/ampdocker:python312 .
+docker build --rm -f AMPDockerFileWine9 -t ssfdre38/ampdocker:wine9 .
 
 #docker push ssfdre38/ampdocker
