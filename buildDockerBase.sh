cbfbb39534c1a3719c3bcaa9c75647f33cf5741a
Daniel Elliott <ssfdre38@msn.com>
2025-05-29 04:23:02 +0000

    update build file

--- buildDockerBase.sh
+++ buildDockerBase.sh
@@ -11,6 +11,6 @@ docker build --rm -f AMPDockerFileGo -t ssfdre38/ampdocker:go .
 docker build --rm -f AMPDockerFileLang -t ssfdre38/ampdocker:lang .
 docker build --rm -f AMPDockerFilePython312 -t ssfdre38/ampdocker:python312 .
 docker build --rm -f AMPDockerFilepython313 -t ssfdre38/ampdocker:python313 .
-docker build --rm -f AMPDockerFileWine10 -t ssfdre38/ampdocker:wine10
+docker build --rm -f AMPDockerFileWine10 -t ssfdre38/ampdocker:wine10 .
 
 #docker push ssfdre38/ampdocker
